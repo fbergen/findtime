f6d2f9f28413c4beadcc068fc4368b33910be720
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2023-11-05 14:57:01 +0100

    more rusty dedup

--- src/libs.rs
+++ src/libs.rs
@@ -1,20 +1,22 @@
 use crate::Event;
 
 pub fn dedup_events(events: &mut Vec<Event>) {
+    // Sort events by start time, and end time
     events.sort_by(|a, b| (a.start, a.end).cmp(&(b.start, b.end)));
-    let mut it: i64 = 0;
 
-    if events.len() < 2 {
-        return;
-    }
-
-    while (it as usize) < events.len() - 2 {
-        let i = it as usize;
-        if events[i].end >= events[i + 1].start || events[i].start == events[i + 1].start {
-            events[i].end = events[i + 1].end;
-            events.remove(i + 1);
-            it -= 1;
+    // Replace the original events with an empty vector and consume it
+    let mut merged_events: Vec<Event> = Vec::new();
+    std::mem::take(events).into_iter().for_each(|event| {
+        match merged_events.last_mut() {
+            // If the last event in `merged_events` overlaps with the current event, merge them
+            Some(last) if last.end >= event.start => {
+                last.end = std::cmp::max(last.end, event.end);
+            }
+            // Otherwise, push the current event to `merged_events`
+            _ => merged_events.push(event),
         }
-        it += 1;
-    }
+    });
+
+    // Replace the original events with the merged ones
+    *events = merged_events;
 }
